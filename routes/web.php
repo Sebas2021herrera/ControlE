c297a3020553252b3410efd97b010caf209afa89
MICHAEL SEBASTIAN HERRERA CRISTIANO <86730541+Sebas2021herrera@users.noreply.github.com>
2024-07-19 14:11:47 -0500

    19-07-2024- se creo ruta, controlador y vista de welcome, se ajusto model role, se ajusto css app,

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+Use App\Http\Controllers\WelcomeController;
 
-
+Route::get('/', [WelcomeController::class, 'index']);
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::get('/create', [AuthController::class, 'createLogin'])->name('create');
+Route::get('/create', [AuthController::class, 'createLogin'])->name('register');
 
 
 
